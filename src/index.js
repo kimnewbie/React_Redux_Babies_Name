bcc134e3005d731206aa536dca2e4537fdbf6e25
kimnewbie <gracecancho@gmail.com>
2021-08-04 00:04:25 +0900

    update

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react'; import ReactDOM from 'react-dom';
 import './index.css';
 import 'antd/dist/antd.css';
 import App from './components/App';
+// Redux 관련 불러오기
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 
+
+// store 생성
 const store = createStore(reducers);
 window.store = store
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={store}> {/* App 컴포넌트가  store에 연동 */}
     <React.StrictMode>
       <App />
     </React.StrictMode>
